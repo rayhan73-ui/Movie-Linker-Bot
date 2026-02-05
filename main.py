eb0c43ef55b515b6960cfcf0dfd0b49c75d5325f
rayhan73-ui <www.rayhan73@gmail.com>
2026-02-06 01:48:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 import logging
-import logging.config
-
-# Get logging configurations
-logging.getLogger().setLevel(logging.INFO)
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
-
+import asyncio
 from pyrogram import Client
 from vars import Var
-from aiohttp import web
+from utils.keepalive import start_server
+
+logging.basicConfig(level=logging.INFO)
 
 class Bot(Client):
     def __init__(self):
@@ -19,19 +13,15 @@ class Bot(Client):
             api_id=Var.API_ID,
             api_hash=Var.API_HASH,
             bot_token=Var.BOT_TOKEN,
-            workers=20,
-            plugins={'root': 'plugins'} # আমরা প্লাগইন সিস্টেম ব্যবহার করব
+            plugins={'root': 'plugins'}
         )
 
     async def start(self):
         await super().start()
+        # ওয়েব সার্ভার শুরু করা যা Render এর পোর্ট এরর ঠিক করবে
+        await start_server()
         me = await self.get_me()
-        print(f"--- Bot Started: {me.first_name} ---")
-        print(f"URL => {Var.URL}")
-
-    async def stop(self, *args):
-        await super().stop()
-        print("--- Bot Stopped ---")
+        print(f"--- {me.first_name} Started Successfully ---")
 
 if __name__ == "__main__":
     bot = Bot()
